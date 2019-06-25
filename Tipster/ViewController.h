511e458c9e61a1fbf8964b418637d95bf4ccee6d
Katelin Chan <kchan23@fb.com>
2019-06-25 14:04:01 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by kchan23 on 6/25/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tipster
+//
+//  Created by kchan23 on 6/25/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
